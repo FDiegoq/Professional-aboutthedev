03efd99166ed034d1664d2a7e6053c8b3237f6b3
Diego <fdiegofreire@gmail.com>
2024-08-20 22:02:51 -0300

    models setup

--- curriculo/models.py
+++ curriculo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Habilidade(models.Model):
+    titulo=models.CharField(max_length=120)
+    is_learning=models.BooleanField()
+    pic=models.ImageField()
+
+class Curso(models.Model):
+    titulo=models.CharField(max_length=250)
+    instituicao=models.CharField(max_length=120)
+    pic=models.ImageField()
+
+
