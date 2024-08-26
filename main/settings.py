89ff13a1db0ffcd8bc2962f3ae97bc73433f6000
Diego <fdiegofreire@gmail.com>
2024-08-26 12:30:55 -0300

    Pillow Setup

--- main/settings.py
+++ main/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
-from pathlib import Path
+from pathlib import Path, os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -111,6 +111,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media/')
 
 
 # Static files (CSS, JavaScript, Images)
