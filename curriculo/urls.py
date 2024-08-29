ab4f9a07ccbb800bb971d8764ea55797e9b152cd
Diego <fdiegofreire@gmail.com>
2024-08-28 21:40:23 -0300

    correção erro no pillow

--- curriculo/urls.py
+++ curriculo/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import index
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('', index, name='index')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
