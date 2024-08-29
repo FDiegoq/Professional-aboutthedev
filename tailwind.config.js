78869b70761e3e195c099cfccf35be1109343cb4
Diego <fdiegofreire@gmail.com>
2024-08-28 21:34:20 -0300

    foto carregada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,9 @@
 module.exports = {   
     content: ['./src/**/*.{html,js}'],   
     theme: {}, 
+    extend: {
+        backgroundImage: {
+          'pfp': "url('static/src/assets/profilepic.jpg')",
+        },
+    }
 }; 
